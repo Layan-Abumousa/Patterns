60ff61a00d374c79781adad29ab1115b2534d5a0
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-08-04 00:25:38 +0300

    .

---
+++ 27.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+/* 
+
+	    0
+  	1 0 1
+  2 2 0 2 2
+3 3 3 0 3 3 3
+
+*/
+int main()
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 7; j++)
+		{
+			if (j + i >= 3 && j - i <= 3)
+			{
+				if (j == 3)
+				{
+					cout << 0<<" ";
+				}
+				else
+					cout << i<<" ";
+
+			}
+			else
+
+				cout << "  ";
+
+
+		}
+		cout << endl;
+	}
+
+    
+}
