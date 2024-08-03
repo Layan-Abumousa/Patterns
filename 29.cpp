1de527e26d5e54edf58ada21b2aeb973d38eaa6c
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-08-04 02:39:32 +0300

    .

---
+++ 29.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+/* 
+  
+	    	  * * * * *
+	    	*       *
+	    *       *
+  	*       *
+  *       *
+* * * * *
+
+*/
+int main()
+{
+	for (int i = 0; i < 6; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			if (i+j ==5 || i+j==9 || (j>=5 && j <=9 && i==0)|| (j >= 0 && j <= 4 && i == 5))
+			{
+				cout << "* ";
+
+			}
+			else
+				cout << "  ";
+
+
+		}
+		cout << endl;
+	}
+
+    
+}
