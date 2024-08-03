15ec71f5f112f7023d33460b2ef49bb3470983bd
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-08-04 02:33:38 +0300

    .

---
+++ 28.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+/* 
+ 
+	    A
+  	A B A
+  A B C B A
+A B C D C B A
+
+*/
+int main()
+{
+	int c = 1;
+	for (int i = 0; i < 4; i++)
+	{
+		c = -1;
+		for (int j = 0; j < 7; j++)
+		{
+			if (j + i >= 3 && j - i <= 3)
+			{
+				if (j > 3)
+				{
+					cout<< (char)('A'+ (--c) )<< " ";
+				}
+				else
+					cout << (char)('A' + (++c)) <<" ";
+
+			}
+			else
+
+				cout << "  ";
+
+
+		}
+		cout << endl;
+	}
+
+    
+}
